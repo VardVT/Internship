c2177b837d00288e0915d68f9c7d7d18ca8bb46d
phantrongkhoi1710-svg <phantrongkhoi1710@gmail.com>
2026-08-07 14:08:54 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// base: './' generates relative asset paths so the build works when hosted
-// at https://<user>.github.io/<repo>/ without needing to hardcode the repo name.
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
